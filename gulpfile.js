2ef4b5ba7f9e32eb4fdadfc61080fdf797e3d1e4
Alex Gleason <alex@alexgleason.me>
2016-06-28 17:59:11 -0400

    Added watcher to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 
+var paths = {
+  sass: 'wagtailbettereditor/static_src/wagtailbettereditor/scss/**/*.scss'
+}
+
 gulp.task('compile-sass', function() {
-    return gulp.src('wagtailbettereditor/static_src/wagtailbettereditor/scss/**/*.scss')
-        .pipe(sass({
-            includePaths: ['node_modules'],
-        }))
-        .pipe(gulp.dest('wagtailbettereditor/static/wagtailbettereditor/css'))
+  return gulp.src(paths.sass)
+    .pipe(sass({includePaths: ['node_modules']}))
+    .pipe(gulp.dest('wagtailbettereditor/static/wagtailadmin/css'))
+})
+
+gulp.task('watch', function() {
+  gulp.watch(paths.sass, ['compile-sass'])
 })
 
 gulp.task('default', ['compile-sass'])
